88514549ec3e2b1270a5c1e4778f0a953bc7f5be
ShayneYurnYang <159043356+ShayneYangSchool@users.noreply.github.com>
2024-02-22 09:55:30 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
 		Point point2 = new Point(-3, 4);
 		Line line1 = new Line(1.2, 7.0);
 		Line line2 = new Line(point1, point2);
-		Line.calcSlope(point1,point2);
+		System.out.println(Line.calcSlope(point1,point2));
 	}
 
 }
